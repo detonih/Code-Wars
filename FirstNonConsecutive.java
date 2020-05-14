4415d49e911aeb7e60fc746a14a40413a428ffe2
Henrique Detoni <detonihenrique@gmail.com>
2020-05-13 21:27:49 -0300

    add my first kata in Java language

---
+++ FirstNonConsecutive.java
@@ -0,0 +1,10 @@
+class FirstNonConsecutive {
+    static Integer find(final int[] array) {
+        for(int i = 1; i < array.length; i++){
+          if(array[i] != 1 + array[i-1]) {
+            return array[i];
+          } 
+        }
+        return null;
+    }
+}
